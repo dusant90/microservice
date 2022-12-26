79b1c071508b050a53f9cf076f5c2f52be3c6b69
dusan.tomic <Dusan.Tomic@wolterskluwer.com>
2022-12-26 13:08:18 +0100

    changes for web client and syncronos communication

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
@@ -1,20 +1,27 @@
 package com.programmingtechie.inventoryservice.controller;
 
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.model.Inventory;
 import com.programmingtechie.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        log.info("Success");
         return inventoryService.isInStock(skuCode);
     }
 }
