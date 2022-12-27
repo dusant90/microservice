d86245b2463adcf300cd5736403fd90daed48628
dusan.tomic <ledutomic@gmail.com>
2022-12-27 16:55:55 +0100

    added discovery-server

--- order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.programmingtechie.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,7 +11,8 @@ import java.lang.ref.WeakReference;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClient() {
+        return WebClient.builder();
     }
 }
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClient;
 
     public void placeOrder(OrderRequest orderRequest) throws IllegalAccessException {
         Order order = new Order();
@@ -39,8 +39,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .collect(Collectors.toList());
 
-        InventoryResponse[] inventoryResponses =  webClient.get()
-                .uri("http://localhost:8082/api/inventory",uriBuilder-> uriBuilder.queryParam(
+        InventoryResponse[] inventoryResponses =  webClient.build().get()
+                .uri("http://inventory-service/api/inventory",uriBuilder-> uriBuilder.queryParam(
                             "skuCode" , skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
